423dc6f031baea3b941b55b22b9756b3f6c30c33
Tesar Rahmat Maulana <tesarrm58@gmail.com>
2024-04-17 17:19:56 +0700

    Not finished - 00:05:03 Setting up folder, header, hero component -- kurang `Hero.jsx"

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,7 +2,6 @@
 import React, { FC, useState } from "react"
 import Heading from "./utils/Heading";
 import Header from "./components/Header";
-// import Hero from "./components/Route/Hero";
 
 interface Props { }
 
@@ -22,7 +21,6 @@ const Page: FC<Props> = (props) => {
         setOpen={setOpen}
         activeItem={activeItem}
       />
-      {/* <Hero /> */}
     </div>
   )
 }
