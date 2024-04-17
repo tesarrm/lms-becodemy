338e839eb9524394b646ef6daa411c5ebdcbfc17
Tesar Rahmat Maulana <tesarrm58@gmail.com>
2024-04-17 15:34:08 +0700

    Error fixed -- delete Hero file

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -39,7 +39,63 @@ const Header: FC<Props> = ({ activeItem, setOpen }) => {
                 : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"
                 }`}
             >
-                
+                <div className="w-[94%] 800px:w-[92%] m-auto py-2 h-full">
+                    <div className="w-full h-[79px] flex items-center justify-between p-3">
+                        <div>
+                            <Link href={"/"}
+                                className={`text-[24px] font-Poppins font-[500] text-black dark:text-white`}
+                            >
+                                ELearning
+                            </Link>
+                        </div>
+                        <div className="flex items-center">
+                            <NavItems
+                                activeItem={activeItem}
+                                isMobile={false}
+                            />
+                            <ThemeSwitcher />
+                            {/* only for mobile */}
+                            <div className="800px:hidden">
+                                <HiOutlineMenuAlt3
+                                    size={24}
+                                    className="cursor-pointer dark:text-white text-black"
+                                    onClick={() => setOpenSidebar(true)}
+                                />
+                            </div>
+                            <HiOutlineUserCircle
+                                size={24}
+                                className="hidden 800px:block cursor-pointer dark:text-white text-black"
+                                onClick={() => setOpen(true)}
+                            />
+                        </div>
+                    </div>
+                    {/* mobile sidebar */}
+                    {openSidebar && (
+                        <div
+
+                            className="fixed w-full h-screen top-1 left-0 z-[99999] dark:bg-[unset] bg-[#00000024]"
+                            onClick={handleClose}
+                            id="screen"
+                        >
+                            <div
+                                className="w-[69%] fixed z-[9999999999] h-screen bg-white dark:bg-slate-900 dark:bg-opacity-90 top-0 right-0"
+                            >
+                                <NavItems activeItem={activeItem} isMobile={true} />
+                                <HiOutlineUserCircle
+                                    size={24}
+                                    className="cursor-pointer ml-6 my-2 dark:text-white text-black"
+                                    onClick={() => setOpen(true)}
+                                />
+                                <br />
+                                <br />
+                                <p className="text-[15px] px-2 pl-5 text-black dark:text-white">
+                                    Copyright @ 2023 Elearning
+                                </p>
+                            </div>
+
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     )
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,8 +2,9 @@
 import React, { FC, useState } from "react"
 import Heading from "./utils/Heading";
 import Header from "./components/Header";
+// import Hero from "./components/Route/Hero";
 
-interface Props {}
+interface Props { }
 
 const Page: FC<Props> = (props) => {
   const [open, setOpen] = useState(false)
@@ -21,6 +22,7 @@ const Page: FC<Props> = (props) => {
         setOpen={setOpen}
         activeItem={activeItem}
       />
+      {/* <Hero /> */}
     </div>
   )
 }
